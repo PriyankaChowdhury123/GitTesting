173f561eb44f794fe4d0a0964c740509154ca659
Priyanka Chowdhury <priyankachowdhury8120@gmail.com>
2020-10-01 01:48:15 +0530

    Hello...

---
+++ src/com/git/testing/Test.java
@@ -0,0 +1,18 @@
+package com.git.testing;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age=52;
+		if(age>18) {
+			System.out.println("You are eligible to vote");
+		}
+		else {
+			System.out.println("Sorry!!You are too young to vote");
+		}
+	}
+
+	}
+
+
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module GitTesting {
+}
